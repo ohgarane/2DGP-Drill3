7928e0a3c43c44c4648fe1d9b22cc4d83f56f09e
ohgarane <ohgarane8@naver.com>
2024-09-24 12:11:33 +0900

    Update 2DGP-Drill3.py
    캐릭터의 원운동 구현 성공

--- 2DGP-Drill3.py
+++ 2DGP-Drill3.py
@@ -10,14 +10,17 @@ boy = load_image('character.png')
 def run_circle():
     print('CIRCLE')
 
-    r = 300
-
-    for degree in range(0,360, 5):
+    r,cx, cy = 300, 800//2, 600//2
+    for degree in range(0,360, 3):
+        theta = math.radians(degree)
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+    
         
-    clear_canvas_now()
-    character.draw_now(400,300)
-    delay(0.1)
-    pass
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.1)
+pass
 
 def run_rectangle():
     print('RECTANGLE')
