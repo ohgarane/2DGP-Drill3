880f66dd60468dd45828f1b559c4ee903ff2f80c
ohgarane <ohgarane8@naver.com>
2024-09-24 12:34:15 +0900

    top 움직임 구현

--- 2DGP-Drill3.py
+++ 2DGP-Drill3.py
@@ -6,6 +6,10 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def draw_boy(x,y):
+    clear_canvas_now()
+    boy.draw_now(x,y)
+    delay(0.1)
 
 def run_circle():
     print('CIRCLE')
@@ -15,15 +19,13 @@ def run_circle():
         theta = math.radians(degree)
         x = r * math.cos(theta) + cx
         y = r * math.sin(theta) + cy
-    
-        
-        clear_canvas_now()
-        boy.draw_now(x,y)
-        delay(0.01)
+        draw_boy()
     pass
 
 def run_top():
     print('top')
+    for x in range(0,800,10):
+        draw_boy(x,550)
     pass
 def run_right():
     print('right')
